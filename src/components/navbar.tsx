1fa70f4dc2ee90c3cf91cad4543f91e6e5944048
Lavesh1208 <laveshs1282@gmail.com>
2025-01-15 22:59:05 +0530

    Create Workspace Form

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 			<div className="flex-col hidden lg:flex">
 				<h1 className="text-2xl font-semibold">Home</h1>
 				<p className="text-muted-foreground">
-					Monitor all of your projects and taska here
+					Monitor all of your projects and tasks here
 				</p>
 			</div>
 			<MobileSidebar />
