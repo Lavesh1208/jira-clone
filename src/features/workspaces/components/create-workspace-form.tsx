2425495a25034f31d531e02850b01b0c0bf92f9b
Lavesh1208 <laveshs1282@gmail.com>
2025-01-16 13:29:10 +0530

    Image upload

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { DottedSeparator } from "@/components/doted-separator";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
@@ -13,6 +14,9 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ImageIcon } from "lucide-react";
+import Image from "next/image";
+import { useRef } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { useCreateWorkspace } from "../api/use-create-workspace";
@@ -24,6 +28,8 @@ interface CreateWorkspaceFormProps {
 
 const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
 	const { mutate, isPending } = useCreateWorkspace();
+	const inputRef = useRef<HTMLInputElement>(null);
+
 	const form = useForm<z.infer<typeof createWorkspaceSchema>>({
 		resolver: zodResolver(createWorkspaceSchema),
 		defaultValues: {
@@ -32,7 +38,27 @@ const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
 	});
 
 	const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
-		mutate({ json: values });
+		const finalValues = {
+			...values,
+			image: values.image instanceof File ? values.image : undefined,
+		};
+
+		mutate(
+			{ form: finalValues },
+			{
+				onSuccess: () => {
+					form.reset();
+				},
+			}
+		);
+	};
+
+	const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const file = e.target.files?.[0];
+
+		if (file) {
+			form.setValue("image", file);
+		}
 	};
 
 	return (
@@ -60,6 +86,59 @@ const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
 										</FormControl>
 										<FormMessage />
 									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="image"
+								render={({ field }) => (
+									<div className="flex flex-col gap-y-2">
+										<div className="flex items-center gap-x-5">
+											{field.value ? (
+												<div className="size-[72px] relative rounded-md overflow-hidden">
+													<Image
+														src={
+															field.value instanceof File
+																? URL.createObjectURL(field.value)
+																: field.value
+														}
+														alt="Logo"
+														className="object-cover"
+														fill
+													/>
+												</div>
+											) : (
+												<Avatar className="size-[72px]">
+													<AvatarFallback>
+														<ImageIcon className="size-[36px] text-neutral-400" />
+													</AvatarFallback>
+												</Avatar>
+											)}
+											<div className="flex flex-col">
+												<p className="text-sm">Workspace Icon</p>
+												<p className="text-sm text-muted-foreground">
+													JPG, PNG, SVG or JPEG, max 1MB
+												</p>
+												<input
+													className="hidden"
+													type="file"
+													accept=".jpg, .png, .svg, .jpeg"
+													ref={inputRef}
+													disabled={isPending}
+													onChange={handleImageChange}
+												/>
+												<Button
+													type="button"
+													disabled={isPending}
+													variant="teritary"
+													className="w-fit mt-2"
+													onClick={() => inputRef.current?.click()}
+												>
+													Upload Image
+												</Button>
+											</div>
+										</div>
+									</div>
 								)}
 							/>
 						</div>
